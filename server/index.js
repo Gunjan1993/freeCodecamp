5dad90179a5c748ab4f0493911b05040d6bb3fd3
Gunjan1993 <72492276+Gunjan1993@users.noreply.github.com>
2024-02-15 17:11:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 const corsConfig={
      origin:['https://free-codecamp-frontend.vercel.app/'],
-    methods:["GET","POST","PUT","DELETE"],
+    methods:["GET","POST","PUT","DELETE","OPTIONS"],
     credentials:true
 }
 app.use(cors(corsConfig));
