13385f4aad74478c946a13e71ff846eefb754502
Gunjan1993 <72492276+Gunjan1993@users.noreply.github.com>
2024-02-15 16:01:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors(corsConfig));
 //app.options("",(corsConfig))
 app.use(cookieParser())
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     // Add other CORS headers as needed
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
