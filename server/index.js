809097cd8ba4c2410f410b49b2ef5132ceed0b1a
Gunjan1993 <72492276+Gunjan1993@users.noreply.github.com>
2024-02-15 15:47:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,13 +11,20 @@ const app = express()
 app.use(express.json())
 
 const corsConfig={
-     origin:['https://free-codecamp-frontend.vercel.app/'],
+     origin:['http://localhost:5173'],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }
 app.use(cors(corsConfig));
 //app.options("",(corsConfig))
 app.use(cookieParser())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    // Add other CORS headers as needed
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
 mongoose.connect("mongodb+srv://gunjangyl99:sensation@cluster0.xbrqbfi.mongodb.net/?retryWrites=true&w=majority")
 
